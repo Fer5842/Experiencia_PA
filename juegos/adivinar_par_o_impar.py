2d2cbe5db7b2d1cce20b0778dfa0d180f1229b19
Fer5842 <163327280+Fer5842@users.noreply.github.com>
2024-03-14 13:42:09 +0000

    termine par o impar

--- juegos/adivinar_par_o_impar.py
+++ juegos/adivinar_par_o_impar.py
@@ -1,4 +1,19 @@
 def adivinar_par_o_impar():
+    import random
+    numero=random.randint(1,10)
+    if numero%2==0:
+        par=True
+    else:
+        par=False
+    print('par o impar?')
+    guess=input()
+    if guess=='par' and par==True or guess=='impar' and par==False:
+        print('Felicidades, ganaste :)')
+    else:
+        print('perdiste :(')
+        
+
+
     """
     Esta función representa el juego de adivinar si un número es par o impar.
     Tienes que permitir que el usuario ingrese una de las dos opciones y
