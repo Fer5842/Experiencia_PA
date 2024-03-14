add135b4e690e50a21a38fc3c9f6706cece211d7
Subilla <163363666+Subilla@users.noreply.github.com>
2024-03-14 14:21:00 +0000

    Modifique el archivo, ahora esta terminado

--- juegos/juego_del_dado.py
+++ juegos/juego_del_dado.py
@@ -4,23 +4,19 @@ def juego_del_dado():
     computador = 0
     contador = 0
     while usuario < 30 and computador < 30:
-        a=input('Presiona enter para lanzar el dado.')
+        a=input('\nPresiona enter para lanzar el dado.')
         if a== '':
-            contador += 1
-            if contador%2 != 0:
-                u = random.randint(1,6)
-                usuario += u 
-                print(f'Obtuviste un {u} en el dado, tu suma total es de {usuario} puntos')
+            u = random.randint(1,6)
+            usuario += u 
+            print(f'\nObtuviste un \033[1m{u}\033[0m en el dado, tu suma total es de \033[1m{usuario}\033[0m puntos')
             if usuario >= 30:
-                contador += 1
-                if contador%2 ==0:
-                    c = random.randint(1,6)
-                    computador += c
-                    print(f'El computador obtuvo un {c} en el dado, su suma total es de {computador} puntos')
-    if contador%2 != 0:
-        return f'¡Ganaste! Sumaste {usuario} puntos'
-    else:
-        return f'Ganó el computador con una suma de {computador} puntos :()'
+                print(f'\n¡Ganaste! Sumaste {usuario} puntos')
+        if usuario < 30:
+            c = random.randint(1,6)
+            computador += c
+            print(f'El computador obtuvo un \033[1m{c}\033[0m en el dado, su suma total es de \033[1m{computador}\033[0m puntos')
+            if computador >= 30:
+                print(f'\nGanó el computador con una suma de {computador} puntos :()')
     """
     Esta función tiene que pedirle al usuario que aprete enter para que lance un dado.
     Esto genera un número al azar que se le suma a la puntuación del usuario.
